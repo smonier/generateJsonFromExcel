9e3422121babd25ffc182d19f60cde46c1f21e86
Stephane Monier <smonier@jahia.com>
2024-02-07 11:45:41 +0100

    first commit

---
+++ generate-json.py
@@ -0,0 +1,20 @@
+import pandas as pd
+sheet = pd.read_excel('/Users/monier/Documents/Jahia/Demo/DSFR/dsgouv-icons.xlsx')
+
+json_array = [{
+
+        "value": {
+            "type": "String",
+            "value": row['value']  # Assuming 'value' is the column name in your Excel
+        },
+        "displayValue": row['displayValue'],  # Assuming 'displayValue' is the column name
+        "propertyList": [{
+            "name": "image",
+            "value": row['propertyList.value']  # Adjust the column name as needed
+        }]
+
+} for index, row in sheet.iterrows()]
+
+import json
+with open('output_file.json', 'w') as f:
+    json.dump(json_array, f, indent=4)
